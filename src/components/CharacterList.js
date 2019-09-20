8c9f4266a96d46102d44a289423f043462d38dec
Brenda Joshua <brendajosh.bj@gmail.com>
2019-09-20 10:49:15 +0300

    fetched characters from the API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Axios from 'axios';
+import CharacterCard from './CharacterCard';
 
-export default function CharacterList() {
+const CharacterList = () => {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(({data: {results}}) => setCharacters(results))
+    .catch(setError)
   }, []);
+
+  if (error) {
+    return<div>An error occured!</div>
+  }
+
+  if (!characters) {
+    return <div name="Characters"/>
+  }
 
   return (
     <section className="character-list">
